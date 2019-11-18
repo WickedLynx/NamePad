2a332f2b3993357e7734df7203261990a693aebd
= <=>
2019-11-18 15:05:54 +0100

    Cleaned up unused code. Added a list view with ProfileThumbs

---
+++ components/ProfileThumb.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  View,
+  Image,
+  Text,
+} from 'react-native';
+
+const ProfileThumb = ({uri, name}) => (
+  <View style={{ backgroundColor: 'blue' }}>
+    <Image source={{uri: uri}} 
+      style={{
+        width: 110,
+        height: 110
+      }}
+    />
+    <Text style={{ color: 'white', fontSize: 16, position: 'absolute', bottom: 10, right: 10 }}>{name}</Text>
+  </View>
+)
+
+export default ProfileThumb;
