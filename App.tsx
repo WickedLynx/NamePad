02f1ea98e0d26256e2e8afe35cdc4a49cf86c9ff
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-11-18 15:46:31 +0100

    Format App

--- App.tsx
+++ App.tsx
@@ -1,29 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  StatusBar,
-  FlatList,
-} from 'react-native';
+import {SafeAreaView, View, Text, StatusBar, FlatList} from 'react-native';
 
 import ProfileThumb from './components/ProfileThumb';
 
 const Header = () => (
   <View>
-    <Text style={{ fontSize: 30, fontWeight: 'bold' }}>NamePad</Text>
+    <Text style={{fontSize: 30, fontWeight: 'bold'}}>NamePad</Text>
   </View>
-)
+);
 
 const profiles = [
   {uri: 'https://via.placeholder.com/150.png', name: 'Jeff', id: 0},
@@ -35,16 +19,15 @@ const App = () => {
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <View style={{ paddingHorizontal: 30 }}>
-        <Header />
-        <FlatList
-          data={profiles}
-          renderItem={({item}) => <ProfileThumb {...item} />}
-          horizontal={false}
-          numColumns={2}
-          columnWrapperStyle={{ justifyContent: 'space-between' }}
-        >
-        </FlatList>
+        <View style={{paddingHorizontal: 30}}>
+          <Header />
+          <FlatList
+            data={profiles}
+            renderItem={({item}) => <ProfileThumb {...item} />}
+            horizontal={false}
+            numColumns={2}
+            columnWrapperStyle={{justifyContent: 'space-between'}}
+          />
         </View>
       </SafeAreaView>
     </>
