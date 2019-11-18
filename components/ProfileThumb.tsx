304536c6e1b45e9fa9d78d9a4bc6892cbb73cd20
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-11-18 15:42:44 +0100

    Format ProfileThumb

--- components/ProfileThumb.tsx
+++ components/ProfileThumb.tsx
@@ -1,25 +1,31 @@
 import React, {FC} from 'react';
-import {
-  View,
-  Image,
-  Text,
-} from 'react-native';
+import {View, Image, Text} from 'react-native';
 
 interface ProfileThumbProps {
-  uri: string,
-  name: string
+  uri: string;
+  name: string;
 }
 
 const ProfileThumb: FC<ProfileThumbProps> = ({uri, name}) => (
-  <View style={{ backgroundColor: 'blue' }}>
-    <Image source={{uri: uri}} 
+  <View style={{backgroundColor: 'blue'}}>
+    <Image
+      source={{uri: uri}}
       style={{
         width: 110,
-        height: 110
+        height: 110,
       }}
     />
-    <Text style={{ color: 'white', fontSize: 16, position: 'absolute', bottom: 10, right: 10 }}>{name}</Text>
+    <Text
+      style={{
+        color: 'white',
+        fontSize: 16,
+        position: 'absolute',
+        bottom: 10,
+        right: 10,
+      }}>
+      {name}
+    </Text>
   </View>
-)
+);
 
 export default ProfileThumb;
